62583c7576f03ea1cb6d355c549831d89592836c
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2024-12-23 07:25:20 +0000

    datatype

---
+++ basic/datatype-s.js
@@ -0,0 +1,28 @@
+//primitive
+
+//7 types : string,Number ,boolean,null,undefined,symbol,bigint
+
+const score =100
+const scoreValue= 100.3
+const isloggedIn =false
+const outTemp=null
+let useremail;
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id == anotherid);
+const bigNumber=12525554657n
+
+
+
+//reference(non-primitive)
+//array,objects,functions
+
+const heros=["krrish","doga"]
+let myobj={
+    name:"Manik",
+    age:22,
+}
+const myfunction= function(){
+    console.log("Hello");}
+
+    
