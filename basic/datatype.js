1dd59acf651318084a75f437fc54f5d68f489637
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2024-12-23 06:44:17 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS cose as newer version
+
+//alert("Hello")
+let name ="Manik"
+let age = 22
+let isLoggedIn= false
+
+//number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof age)
