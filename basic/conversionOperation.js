36e048049c605f2016798d10c0b759a00bdae674
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2024-12-23 07:04:44 +0000

    conversion

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -14,3 +14,23 @@ console.log(valueInNumber);
 let isloggedIn =  1
 let boolean=Boolean(isloggedIn)
 console.log(boolean)
+
+
+// *****Operation*****
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+let str1="hello "
+let str2="Manik"
+let str3=str1 +str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(2+2+"1");
+
+
+// console.log(+true)  => 1
