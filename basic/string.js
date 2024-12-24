9a008df26be0efbe91aa01fd2afa428360b2c21c
Manik31Raj <108018610+Manik31Raj@users.noreply.github.com>
2024-12-24 05:56:05 +0000

    string

---
+++ basic/string.js
@@ -0,0 +1,12 @@
+const name="Manik"
+const repocount=50
+
+console.log(name + repocount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String("Manik")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
